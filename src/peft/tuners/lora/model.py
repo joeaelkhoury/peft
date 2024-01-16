84d8d5b5f937434cad9ad49534846215737cc8bf
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-01-16 17:50:44 +0530

    Update model.py

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -611,7 +611,7 @@ class LoraModel(BaseTuner):
             lora_B_deltas.append(current_adapter_lora_B.data)
         valid_weights = torch.tensor(valid_weights).to(lora_A_deltas[0].device)
         lora_deltas = [lora_A_deltas, lora_B_deltas]
-        dtype = lora_A_deltas[0].stype
+        dtype = lora_A_deltas[0].dtype
         for i, task_tensors in enumerate(lora_deltas):
             if combination_type == "linear":
                 lora_deltas[i] = task_arthimetic(task_tensors, valid_weights)
